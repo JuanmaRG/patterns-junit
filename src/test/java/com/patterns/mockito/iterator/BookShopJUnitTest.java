adc39d8312692d714ac5af416e7a38d94f21db2c
Juanma <juanmanuelruizgil@gmail.com>
2021-03-28 17:31:14 +0200

    Solved: fix

--- src/test/java/com/patterns/mockito/iterator/BookShopJUnitTest.java
+++ src/test/java/com/patterns/mockito/iterator/BookShopJUnitTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class BookShopJUnitTest {
+class BookShopJUnitTest {
 
 
     private BookShop bookShop;
