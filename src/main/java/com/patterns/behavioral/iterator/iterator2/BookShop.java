ab13720f5eb86818f93740f6f6f06b5d6920b2d4
Juanma <juanmanuelruizgil@gmail.com>
2021-03-29 22:33:36 +0200

    minor fixed in DBconfigurationmockitotest

--- src/main/java/com/patterns/behavioral/iterator/iterator2/BookShop.java
+++ src/main/java/com/patterns/behavioral/iterator/iterator2/BookShop.java
@@ -5,6 +5,7 @@ import com.patterns.behavioral.iterator.iterator1.Book;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class BookShop implements Iterable<Book>{
 
@@ -36,7 +37,7 @@ public class BookShop implements Iterable<Book>{
         }
 
         @Override
-        public Book next() {
+        public Book next() throws NoSuchElementException {
             return books.get(currentIndex++);
         }
     }
